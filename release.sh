27d4ba76ee051b8fc62784d809a2991fdc1554b6
jimmy <jimmy_ysy@163.com>
2021-06-07 11:06:11 +0800

    完善shell

--- release.sh
+++ release.sh
@@ -2,6 +2,18 @@
 echo "开始执行发布程序"
 git pull
 git add .
+git status
+
+while true;do
+    read -p "请输入提交文件：" dirName
+    git add $dirName
+    read -p "继续提交？（y/n）：" check
+    if (($check == 'n'))
+    then
+    break
+    fi
+done
+
 read -p "请输入提交内容：" commit
 git commit -m $commit
 git push
