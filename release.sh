6f01594727905b08df1cf46fb9cf403c2def7f46
jimmy <jimmy_ysy@163.com>
2021-06-08 15:50:41 +0800

    修改git命令

--- release.sh
+++ release.sh
@@ -3,16 +3,16 @@ echo "开始执行发布程序"
 git pull
 git status
 
-while true;do
-    read -p "请输入提交文件：" dirName
-    git add $dirName
-    read -p "继续提交？（y/n）：" check
-    if (($check == 'n'))
-    then
-    break
-    fi
-done
-
+# while true;do
+#     read -p "请输入提交文件：" dirName
+#     git add $dirName
+#     read -p "继续提交？（y/n）：" check
+#     if (($check == 'n'))
+#     then
+#     break
+#     fi
+# done
+git add -p
 read -p "请输入提交内容：" commit
 git commit -m $commit
 git push
